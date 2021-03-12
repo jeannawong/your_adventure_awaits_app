87e8998aec52cb92323e8860a065d019ab8b73ba
jeannawong <jeannawong25@gmail.com>
2021-03-11 21:15:13 -0600

    Made changes to orders_controller.rb

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -8,15 +8,15 @@ class Api::OrdersController < ApplicationController
   def create
     # Open the products table
     # Find the product with an id that matches the product_id given by the user
-    product = Journey.find_by(id: params[:product_id])
+    journey = Journey.find_by(id: params[:journey_id])
     # Get the price from the product and multiply it by the quantity given by the user
-    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_subtotal = journey.price
     # Calculate tax and total
     calculated_tax = calculated_subtotal * 0.09
     calculated_total = calculated_subtotal + calculated_tax
     # Save the order in the database
     @order = Order.new(
-      user_id: current_user.id,
+      user_id: params[:user_id],
       journey_id: params[:journey_id],
       review_rating: params[:review_rating],
       review_comment: params[:review_comment],
@@ -29,7 +29,7 @@ class Api::OrdersController < ApplicationController
   end
 
   def show
-    @order = current_user.orders.find_by(id: params[:id])
+    @order = Order.find_by(id: params[:id])
     render "show.json.jb"
   end
 
